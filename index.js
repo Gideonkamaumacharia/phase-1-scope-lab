4a16631466d8bcb1095d987ce53f94db0fd86289
Gideon Macharia <kamaugedion95@gmail.com>
2024-03-22 08:41:38 +0300

    test scope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+  let customerName = customerName.toUpperCase();
+    return customerName;
+    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+   return bestCustomer ='maybe bob';
+}
+
+const leastFavoriteCustomer = 'sarah'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'serah';
+}
+
+upperCaseCustomerName()
+setBestCustomer()
+overwriteBestCustomer()
+changeLeastFavoriteCustomer()
